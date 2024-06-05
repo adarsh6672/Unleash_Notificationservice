d3e30daf4f41d104dfad85d5a7879e104e451d58
adarsh6672 <adarshkeruvatty@gmail.com>
2024-06-05 18:23:39 +0530

    updation new1

--- src/main/java/com/unleash/notification_service/config/KafkaConsumerConfig.java
+++ src/main/java/com/unleash/notification_service/config/KafkaConsumerConfig.java
@@ -28,7 +28,7 @@ public class KafkaConsumerConfig {
     public Map<String, Object> consumerConfig() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "kafka:9092");
+                bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
