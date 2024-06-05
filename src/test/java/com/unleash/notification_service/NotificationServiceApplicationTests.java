bf820d1b9d3d0d8f6a775c42f0f0729f52a95011
adarsh6672 <adarshkeruvatty@gmail.com>
2024-06-05 18:00:51 +0530

    updation new

--- src/test/java/com/unleash/notification_service/NotificationServiceApplicationTests.java
+++ src/test/java/com/unleash/notification_service/NotificationServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.unleash.notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class NotificationServiceApplicationTests {
 
